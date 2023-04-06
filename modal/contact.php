3d57a0b3d11f429a94e6233aad7bbfe855ea6c04
bastiengrn <bastien.garnier@mmibordeaux.com>
2023-04-06 17:51:12 +0200

    php

--- modal/contact.php
+++ modal/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // Put contacting email here
-$php_main_email = "bastiengrn2519@gmail.com";
+$php_main_email = "bastien.garnier@mmibordeaux.com";
 
 //Fetching Values from URL
 $php_name = $_POST['ajax_name'];
